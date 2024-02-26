bf9acd5c0465de3f106cd3e212091c8cda1c6f9f
ChrissiKalk97 <kalk@med.uni-frankfurt.de>
2024-02-26 15:07:15 +0100

    Plotting unique regions according to ORF number corrected

--- Output/check_results/positions_of_unique_Regions/unique_regions_per_split_ORF_transcript.py
+++ Output/check_results/positions_of_unique_Regions/unique_regions_per_split_ORF_transcript.py
@@ -1,10 +1,15 @@
 #unique_region_locations.py takes as input a unique regions file from a
 #run of the SplitORF pipeline and calculates the position of the unique regions 
 #with respect to their ORF, the start in stop positions of the unique region
-#with respect tot he ORf are plotted as density plots
+#with respect to the ORF are plotted as density plots
 #The type of unique regions needs to be specified as DNA or protein
 
-#usage: python unique_region_locations.py Unique_{type}_Regions_merged_{for type protein: valid}.bed type
+#usage: python unique_regions_per_split_ORF_transcript.py 
+#directory_pipe_output/UniqueProteinMatches.bed
+#directory_pipe_output/Unique_DNA_Regions_merged.bed region_type transcript_type
+
+#with region type DNa or protein
+#transcript type RNA or NMD
 
 
 import sys
@@ -13,13 +18,28 @@ import seaborn as sbn
 import matplotlib.pyplot as plt
 
 def main():
-    valid_ORFs = pd.read_csv(sys.argv[1], sep =":|\t")
+    valid_ORFs = pd.read_csv(sys.argv[1], sep =":|\t", header = None,\
+            names = ["target", "protAlignPos_start", "protAlignPos_end", "NMD_transcript", "ORF_nr"])
+    #UniqueProteinMatches.bed
+    print(valid_ORFs.head())
+    print(len(valid_ORFs["ORF_nr"]))
 
     unique_regions = pd.read_csv(sys.argv[2], sep =":|\t", \
         names=["gene_transcript", "ORF_nr", "ORF_start", "ORF_end", "unique_start", "unique_end"],\
             engine='python')
-    unique_regions["ORF_start"] = unique_regions["ORF_start"].astype(int)
-    unique_regions = unique_regions.sort_values(['gene_transcript', 'ORF_start'])
+    print(unique_regions.head())
+    print(len(unique_regions["ORF_nr"]))
+
+    #print("shit", [r for r in unique_regions["ORF_nr"].unique() if r not in valid_ORFs["ORF_nr"].unique()])
+    
+    unique_regions = pd.merge(valid_ORFs, unique_regions, on="ORF_nr", how="outer")
+    unique_regions = unique_regions[unique_regions['protAlignPos_start'].notnull()]
+    #unique_regions["ORF_start"] = unique_regions["ORF_start"].astype(int)
+    #unique_regions["protAlignPos_start"] = unique_regions["protAlignPos_start"].astype(int)
+    unique_regions = unique_regions.sort_values(['NMD_transcript', 'protAlignPos_start'])
+    print(unique_regions.head())
+    print(unique_regions.tail())
+    
     
     region_type = sys.argv[3]
     if region_type == "protein":
@@ -34,50 +54,47 @@ def main():
     unique_regions["stop_percent"] = (unique_regions["unique_end"])/unique_regions["ORF_length"]
 
 
-    unique_regions['ORFcounter'] = unique_regions.groupby(['gene_transcript','ORF_start']).cumcount()+1
-    #unique_regions['ORFcounter'] = unique_regions['ORFcounter'].astype('category')
+    unique_regions['ORFcounter'] = unique_regions.groupby(['NMD_transcript']).cumcount()+1
     print(unique_regions.head())
     print(unique_regions.tail())
+    unique_regions['ORFcounter'] = unique_regions['ORFcounter'].astype('category')
+    
 
     transcript_type = sys.argv[4]
 
+
+    palette = sbn.color_palette("tab10") + sbn.color_palette("viridis", n_colors = 15)
     sbn.histplot(data = unique_regions, x = 'stop_percent', hue = 'ORFcounter', multiple="stack",\
-                  palette=["C0", "C1", "C2", "C3", "C4", "C5", "C6"])\
+                  palette=palette)\
         .set(title=f'Histogram of end positions of unique {transcript_type}_{region_type} regions, by ORF number')
     plt.savefig(f"unique_regions_by_ORF_number/stop_positions_hist_by_ORF_number_{transcript_type}_{region_type}_stack.png")
     plt.close()
 
     sbn.histplot(x = unique_regions["start_percent"], hue = unique_regions['ORFcounter'], multiple="stack", \
-                  palette=["C0", "C1", "C2", "C3", "C4", "C5", "C6"])\
+                  palette=palette)\
         .set(title=f'Histogram of start positions of unique {transcript_type}_{region_type} regions, by ORF number')
     plt.savefig(f"unique_regions_by_ORF_number/start_positions_hist_by_ORF_number_{transcript_type}_{region_type}_stack.png")
     plt.close()
 
-    sbn.histplot(data = unique_regions, x = 'stop_percent', hue = 'ORFcounter', multiple="dodge",\
-                  palette=["C0", "C1", "C2", "C3", "C4", "C5", "C6"])\
+    sbn.histplot(data = unique_regions, x = 'stop_percent', hue = 'ORFcounter', multiple="dodge", binwidth=0.8, \
+                  palette=palette)\
         .set(title=f'Histogram of end positions of unique {transcript_type}_{region_type} regions, by ORF number')
     plt.savefig(f"unique_regions_by_ORF_number/stop_positions_hist_by_ORF_number_{transcript_type}_{region_type}_dodge.png")
     plt.close()
 
-    sbn.histplot(x = unique_regions["start_percent"], hue = unique_regions['ORFcounter'], multiple="dodge", \
-                  palette=["C0", "C1", "C2", "C3", "C4", "C5", "C6"])\
+    sbn.histplot(x = unique_regions["start_percent"], hue = unique_regions['ORFcounter'], multiple="dodge", binwidth=0.8, palette = palette)\
         .set(title=f'Histogram of start positions of unique {transcript_type}_{region_type} regions, by ORF number')
+    
     plt.savefig(f"unique_regions_by_ORF_number/start_positions_hist_by_ORF_number_{transcript_type}_{region_type}_dodge.png")
     plt.close()
 
 
-    sbn.histplot(unique_regions.groupby("gene_transcript").count()["ORF_nr"])\
+    sbn.histplot(unique_regions.groupby("NMD_transcript").count()["ORF_nr"], binwidth=0.8)\
         .set(title=f'Number of unique regions per transcript',\
               xlabel="Nr of unique regions per transcript")
     plt.savefig(f"unique_regions_by_ORF_number/nr_of_unique_{region_type}_regions_per_transcripts_{transcript_type}.png")
     plt.close()
 
-    """fig, ((ax0, ax1)) = plt.subplots(nrows=1, ncols=2)
-
-    colors = ['red', 'tan', 'lime']
-    ax0.hist(x = unique_regions["start_percent"], 100, density=True, histtype='bar', color=colors, label=colors)
-    ax0.legend(prop={'size': 10})
-    ax0.set_title('bars with legend')"""
 
 
 if __name__ == "__main__":
