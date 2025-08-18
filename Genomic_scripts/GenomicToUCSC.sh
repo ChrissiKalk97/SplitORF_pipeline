e8cb5525f20f14e0b724aa4d9c99ba0f7e6903fa
ChrissiKalk97 <kalk@med.uni-frankfurt.de>
2025-08-18 09:37:47 +0200

    remove unnecessary scripts

--- Genomic_scripts/GenomicToUCSC.sh
+++
@@ -1,7 +0,0 @@
-in=$1
-ucOut=$(echo $in | rev | cut -f 2- -d '.' | rev)_UCSC.bed
-scafOut=$(echo $ucOut | rev | cut -f 2- -d '.' | rev)_no_scaffold.bed
-
-source ./Genomic_scripts/ScaffoldUmrechnung.sh $in
-./Genomic_scripts/chromToUcsc -a ./Genomic_scripts/chromAlias_38p14_18_12_23.tsv -i $in -o $ucOut
-cat $ucOut | awk '{if(index($1,"_")>0)print substr($1,1,index($1,"_")-1),$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12; else print $1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12}' > $scafOut
