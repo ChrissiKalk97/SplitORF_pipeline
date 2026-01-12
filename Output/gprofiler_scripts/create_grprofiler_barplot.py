a8e37c43ffc1770fb58cab435bb854a077d71edd
ChrissiKalk97 <kalk@med.uni-frankfurt.de>
2026-01-12 16:31:44 +0100

    gprofiler barplot update

--- Output/gprofiler_scripts/create_grprofiler_barplot.py
+++ Output/gprofiler_scripts/create_grprofiler_barplot.py
@@ -27,23 +27,41 @@ def parse_arguments():
     parser.add_argument(
         "sep",
         type=str,
-        help="Path to the output plot file (SVG)"
+        help="separator"
+    )
+
+    parser.add_argument(
+        "transcript_type",
+        type=str,
+        help="separator"
     )
 
     return parser.parse_args()
 
 
-def main(go_terms, out_plot, sep):
-    go_terms_df = pd.read_csv(go_terms, sep=sep, header=0)
+def main(go_terms, out_plot, sep, transcript_type):
+    go_terms_df = pd.read_csv(go_terms, sep=sep, header=0, decimal=',')
     go_terms_df = go_terms_df.sort_values(
         by='negative_log10_of_adjusted_p_value', ascending=False)
-    g = sbn.catplot(data=go_terms_df, x="negative_log10_of_adjusted_p_value",
-                    y="term_name", kind="bar", palette="viridis", height=6, aspect=1.5)
+    g = sbn.catplot(data=go_terms_df,
+                    x="negative_log10_of_adjusted_p_value",
+                    y="term_name",
+                    kind="bar",
+                    palette="viridis",
+                    # orient="h",
+                    height=6,
+                    aspect=1.5)
+    plt.xticks(rotation=45, ha='right')
     g.set_axis_labels("−log₁₀(adjusted p-value)", "GO Term")
+    # Add title
+    g.fig.suptitle(
+        f"GO term enrichment for {transcript_type} transcripts", fontsize=16)
 
+    # Optional: adjust space so title doesn't overlap
+    g.fig.subplots_adjust(top=0.9)
     plt.savefig(f'{out_plot}', format="svg")
 
 
 if __name__ == "__main__":
     args = parse_arguments()
-    main(args.go_terms, args.out_plot, args.sep)
+    main(args.go_terms, args.out_plot, args.sep, args.transcript_type)
